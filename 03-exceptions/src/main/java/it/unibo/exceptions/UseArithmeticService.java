c9eb611e6a7d2ab3fcb92ee0eda82529e4daebb4
Riccardo Medri <riccardomedri9@gmail.com>
2026-03-10 15:13:17 +0100

    Retry network operations on IO exceptions

--- 03-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
+++ 03-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
@@ -3,6 +3,7 @@ package it.unibo.exceptions;
 import it.unibo.exceptions.fakenetwork.api.NetworkComponent;
 import it.unibo.exceptions.fakenetwork.impl.ServiceBehindUnstableNetwork;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static it.unibo.exceptions.arithmetic.ArithmeticService.DIVIDED;
@@ -48,6 +49,15 @@ public final class UseArithmeticService {
          * This method should re-try to send message to the provided server, catching all IOExceptions,
          * until it succeeds.
          */
+        boolean completed = false;
+        while (!completed) {
+            try {
+                server.sendData(message);
+                completed = true;
+            } catch (final IOException e) {
+                // Retry until the network answers
+            }
+        }
     }
 
     private static String retryReceiveOnNetworkError(final NetworkComponent server) {
@@ -55,7 +65,13 @@ public final class UseArithmeticService {
          * This method should re-try to retrieve information from the provided server, catching all IOExceptions,
          * until it succeeds.
          */
-        return null;
+        while (true) {
+            try {
+                return server.receiveResponse();
+            } catch (final IOException e) {
+                // Retry until the network answers
+            }
+        }
     }
 
     private static void assertEqualsAsDouble(final String expected, final String actual) {
